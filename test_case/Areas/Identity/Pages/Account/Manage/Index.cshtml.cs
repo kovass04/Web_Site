c56006cb8258fb03e6342770eb3fd9be84eddc11
Oleh <kovass04@gmail.com>
2022-07-23 18:59:59 +0300

    add new bugs login and add rename nikname

--- test_case/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ test_case/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -56,21 +56,21 @@ namespace test_case.Areas.Identity.Pages.Account.Manage
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Phone]
-            [Display(Name = "Phone number")]
-            public string PhoneNumber { get; set; }
+            
+            [Display(Name = "Name")]
+            public string Username { get; set; }
         }
 
         private async Task LoadAsync(test_caseUser user)
         {
             var userName = await _userManager.GetUserNameAsync(user);
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+
 
             Username = userName;
 
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                Username = userName
             };
         }
 
@@ -100,19 +100,15 @@ namespace test_case.Areas.Identity.Pages.Account.Manage
                 return Page();
             }
 
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            if (Input.PhoneNumber != phoneNumber)
+            var userSet = await _userManager.GetUserNameAsync(user);
+            if (Input.Username != userSet)
             {
-                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
-                if (!setPhoneResult.Succeeded)
-                {
-                    StatusMessage = "Unexpected error when trying to set phone number.";
-                    return RedirectToPage();
-                }
+                var setUserResult = await _userManager.SetUserNameAsync(user, Input.Username);
+               
             }
 
             await _signInManager.RefreshSignInAsync(user);
-            StatusMessage = "Your profile has been updated";
+            StatusMessage = "Ваш профіль оновлено";
             return RedirectToPage();
         }
     }
